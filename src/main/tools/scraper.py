b9168ff936de845dcb7c0dc0d9fe26cca8466153
alex <a.gilmore1993@gmail.com>
2023-11-09 21:43:51 -0600

    scraper progress

--- src/main/tools/scraper.py
+++ src/main/tools/scraper.py
@@ -1,7 +1,9 @@
 from typing import Callable, Dict
 from typing import List, Tuple
 import os
+import sys
 import io
+import requests
 
 import pandas as pd
 # from langchain.schema.document import Document
@@ -11,12 +13,13 @@ from google.oauth2 import service_account
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 
+
 class Scraper:
     """
     This class provides methods for scraping transcripts from YouTube and loading new data into storage buckets.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, channel_id: str = None, uploads_id: str = None) -> None:
        
         credentials = service_account.Credentials.from_service_account_file(
                 os.environ.get('GCP_SERVICE_ACCOUNT_KEY_PATH')
@@ -31,9 +34,26 @@ class Scraper:
         # This can happen with unaired live videos
         self._unsuccessful_list = None
 
-        self._channel_id = None
-        self._uploads_id = None
+        self._channel_id = self._get_channel_id if not channel_id else channel_id
+        self._uploads_id = self._get_uploads_id if not uploads_id else uploads_id
 
+
+    @property
+    def channel_id(self) -> str:
+        return self._channel_id
+    
+    @channel_id.setter
+    def channel_id(self, channel_id: str) -> None:
+        self._channel_id = channel_id
+    
+    @property
+    def uploads_id(self) -> str:
+        return self._uploads_id
+    
+    @uploads_id.setter
+    def uploads_id(self, uploads_id: str) -> None:
+        self._uploads_id = uploads_id
+    
     def add_new_youtube_urls(self) -> None:
         """
         This method checks for new youtube videos and updates the csv file in GCP Storage.
@@ -80,7 +100,7 @@ class Scraper:
         file_loc = "youtube/transcripts/"
         self.bucket.blob(file_loc+video_id+".txt").upload_from_string(transcript, 'text/plain')
 
-    def create_and_upload_transcripts(self) -> List[str]:
+    def create_and_upload_transcripts(self) -> None:
         """
         This method:
         1. gets the list of all video urls from GCP Storage.
@@ -104,25 +124,58 @@ class Scraper:
                 # print(e)
                 unsuccessful+=[id]
 
-        return unsuccessful  
+        self._unsuccessful_list = unsuccessful  
 
-    def update_unsuccessful_transcripts(self, unsuccessful_list: List[str]) -> None:
+    def update_unsuccessful_transcripts(self) -> None:
         """
         This method takes a list of unsuccessful YouTube ids and creates a new csv file
         in GCP Storage for later retrieval. 
         """
 
         file_loc = "youtube/"
-        failed_df = pd.Series({"YouTube_Address": unsuccessful_list})
+
+        # create failed df
+        failed_df = pd.Series({"YouTube_Address": self._unsuccessful_list})
+
+        # check if failed df in storage
+
+        # pull failed df from storage
+
+        # append new failed df
+
+        # upload new df to storage
         self.bucket.blob(file_loc+"failed_video_list.csv").upload_from_string(failed_df.to_csv(), 'text/csv')
 
-    def scrape_video_addresses(next_page_token: str = None, total_results: int = -1, videos: List[str] = []) -> List[str]:
-            
-        channel_id = "UCvze3hU6OZBkB1vkhH2lH9Q"
-        api_key = os.environ.get("YOUTUBE_API_KEY")
-        uploads_id = "UUvze3hU6OZBkB1vkhH2lH9Q" # uploads playlist id
+    @staticmethod
+    def _get_channel_id() -> str:
+        """
+        This method retrieves the channel id for the "The Needle Drop" YouTube channel.
+        """
 
-        address = f"https://www.googleapis.com/youtube/v3/playlistItems?playlistId={uploads_id}&key={api_key}&part=snippet&maxResults=50"
+        address = f"https://www.googleapis.com/youtube/v3/results?search_query=the+needle+drop&key={os.environ.get('YOUTUBE_API_KEY')}&part=snippet"
+        req = requests.get(address)
+        data = req.json()
+
+        return data['items'][0]['snippet']['channelId']
+    
+    def _get_uploads_id(self) -> str:
+        """
+        This method retrieves the uploads id for the channel of interest.
+        """
+
+        address = f"https://www.googleapis.com/youtube/v3/channels?id={self._channel_id}&key={os.environ.get('YOUTUBE_API_KEY')}&part=contentDetails"
+        try:
+            req = requests.get(address)
+            data = req.json()
+        except ValueError as e:
+            print(e, "Provide accurate channel id.")
+
+        return data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+
+    def scrape_video_addresses(self, next_page_token: str = None, total_results: int = -1, videos: List[str] = []) -> List[str]:
+            
+        address = f"https://www.googleapis.com/youtube/v3/playlistItems?playlistId={self._uploads_id}&key={os.environ.get('YOUTUBE_API_KEY')}&part=snippet&maxResults=50"
         
         if not next_page_token:
             vid_req = requests.get(address)
@@ -147,6 +200,6 @@ class Scraper:
         
         next_page_token = vids['nextPageToken']
 
-        get_video_addresses(next_page_token=next_page_token, total_results=total_results, videos=videos)
+        self.scrape_video_addresses(next_page_token=next_page_token, total_results=total_results, videos=videos)
 
         return videos
